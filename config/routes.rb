10270a45881058ac4c95eb5e8fd29aecfd02f420
arvind4501 <arvindjangirlpu@gmail.com>
2024-02-16 21:01:35 +0530

    added turbo and hotwire and confifured redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  resources :rooms
+  resources :users
+  root 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
